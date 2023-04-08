de72829177f3ae80c45f461f566fe87b07bb2c60
Rocky <atoyanrobert21@gmail.com>
2023-04-08 15:23:21 +0300

    Create app

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -7,7 +7,7 @@ export const Intro:FC<{title?:string,subtitle?:string}> = ({title,subtitle}) => 
 
     return  <div className="intro">
         <div className="intro__img ibg">
-            <img src="../../assets/images/brand/Brand_02.png" alt=""/>
+            <img src="../assets/images/brand/Brand_02.png" alt=""/>
         </div>
         <div className="intro__content">
             <h1 className="intro__title">{tempTitle.length > 1 ? <><span>{tempTitle[0]}</span> {tempTitle.slice(1).join(' ')}</> : tempTitle}</h1>
